dbd12df4d4abd9ef0a54ad63c06dd9bb1b4b15bb
thiagoassuncao774-maker <thiagoassuncao774@gmail.com>
2026-04-07 19:56:02 -0300

    Add files via upload

---
+++ bloco 2/exercicio11.py
@@ -0,0 +1,10 @@
+numero1=int(input("digite aqui o primeiro numero: "))
+numero2=int(input("digite aqui o segundo numero: "))
+soma = numero1+numero2
+subtração = numero1-numero2
+divisão = numero1/numero2
+multiplicação = numero1*numero2
+print("soma", soma)
+print ("subtração", subtração)
+print ("divisão", divisão)
+print("multiplicação", multiplicação)
